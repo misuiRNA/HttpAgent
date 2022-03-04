8004f1ee32d6bb8a7e5cd5cf2cab351d60fb7f37
Ni Rui <nirui@datagrand.com>
2022-03-04 17:40:01 +0800

    [issues] support GET without params

--- src/main/java/agent/interceptor/HttpRoutingInterceptor.java
+++ src/main/java/agent/interceptor/HttpRoutingInterceptor.java
@@ -1,6 +1,9 @@
 package agent.interceptor;
 
 import agent.service.HttpRoutingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.*;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,11 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Writer;
 
-import static utils.AnsiString.*;
+import static utils.AnsiString.green;
+import static utils.AnsiString.red;
 
 public class HttpRoutingInterceptor implements HandlerInterceptor {
 
-    private HttpRoutingService routingService;
+    private final HttpRoutingService routingService;
 
     public HttpRoutingInterceptor(HttpRoutingService routingService) {
         this.routingService = routingService;
@@ -22,25 +26,46 @@ public class HttpRoutingInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String targetUrl = routingService.routeUrl(request);
         if (null == targetUrl) {
-            System.out.println("[routing] " + red("match routing rule failed!"));
+            log(red("match routing rule failed! " + request.getRequestURI()));
             return true;
         }
-        System.out.println("[routing] " + green(String.format("match routing rule success: %s ==>> %s",
-                                         request.getRequestURI(),
-                                         targetUrl)));
+        log(green(String.format("match routing rule success: %s ==>> %s", request.getRequestURI(), targetUrl)));
 
-        responseWithString(response, targetUrl);
+        ResponseEntity<String> returnValue = null;
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            returnValue = restTemplate.getForEntity(targetUrl, String.class);
+        } catch (HttpStatusCodeException e) {
+            returnValue = new ResponseEntity<String>(e.getResponseBodyAsString(), e.getStatusCode());
+            e.printStackTrace();
+        } catch (ResourceAccessException e) {
+            returnValue = new ResponseEntity<String>("Connection refused! Can't find service(s)", HttpStatus.BAD_GATEWAY);
+            e.printStackTrace();
+        } catch (RestClientException e) {
+            returnValue = new ResponseEntity<String>("Unknown Error!", HttpStatus.INTERNAL_SERVER_ERROR);
+            e.printStackTrace();
+            throw e;
+        } finally {
+            fillResponse(response, returnValue);
+            log("exchanged! http code is [" + response.getStatus() + "]");
+        }
         return false;
     }
 
-    private void responseWithString(HttpServletResponse response, String content) {
+    private void fillResponse(HttpServletResponse response, ResponseEntity<String> returnValue) {
+        response.setStatus(returnValue.getStatusCodeValue());
+
         try {
             Writer writer = response.getWriter();
-            writer.write(content);
+            writer.write(returnValue.getBody());
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void log(String msg) {
+        System.out.println("[routing] " + msg);
     }
 
 }
