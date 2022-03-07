b31ba995027de610e403d62500023da4c797276c
Ni Rui <nirui@datagrand.com>
2022-03-07 19:40:30 +0800

    [issues] support POST with params

--- src/main/java/agent/interceptor/HttpRoutingInterceptor.java
+++ src/main/java/agent/interceptor/HttpRoutingInterceptor.java
@@ -1,12 +1,15 @@
 package agent.interceptor;
 
 import agent.service.HttpRoutingService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +18,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.net.URLDecoder;
+import java.util.*;
 
 import static utils.AnsiString.green;
 import static utils.AnsiString.red;
@@ -34,13 +38,23 @@ public class HttpRoutingInterceptor implements HandlerInterceptor {
             log(red("match routing rule failed! " + request.getRequestURI()));
             return true;
         }
-        targetUrl += getQueryString(request);
+        targetUrl += genQueryString(request);
         log(green(String.format("match routing rule success: %s ==>> %s", request.getRequestURI(), targetUrl)));
 
-        ResponseEntity<String> returnValue = null;
-        RestTemplate restTemplate = new RestTemplate();
+        HttpMethod method = HttpMethod.valueOf(request.getMethod());
+        HttpHeaders headers = genHeaders(request);
+
+        Object body = null;
         try {
-            returnValue = restTemplate.getForEntity(targetUrl, String.class);
+            body = genHttpRequestBody(request);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        HttpEntity<Object> requestEntity = new HttpEntity<Object>(body, headers);
+
+        ResponseEntity<String> returnValue = null;
+        try {
+            returnValue = new RestTemplate().exchange(targetUrl, method, requestEntity, String.class);
         } catch (HttpStatusCodeException e) {
             returnValue = new ResponseEntity<String>(e.getResponseBodyAsString(), e.getStatusCode());
             e.printStackTrace();
@@ -58,7 +72,19 @@ public class HttpRoutingInterceptor implements HandlerInterceptor {
         return false;
     }
 
-    private String getQueryString(HttpServletRequest request) {
+    private Object genHttpRequestBody(HttpServletRequest request) throws IOException {
+        if (request instanceof StandardMultipartHttpServletRequest) {
+            MultiValueMap<String, Object> formatBody = new LinkedMultiValueMap<String, Object>();
+            for (Map.Entry<String, String[]> paramEntity : request.getParameterMap().entrySet()) {
+                formatBody.addAll(paramEntity.getKey(), Arrays.asList(paramEntity.getValue()));
+            }
+            return formatBody;
+        } else {
+            return StreamUtils.copyToByteArray(request.getInputStream());
+        }
+    }
+
+    private String genQueryString(HttpServletRequest request) {
         String queryStr = request.getQueryString();
         if (null == queryStr || 0 == queryStr.length()) {
             return "";
@@ -71,7 +97,15 @@ public class HttpRoutingInterceptor implements HandlerInterceptor {
         }
         return String.format("?%s", queryStr);
     }
-
+    private HttpHeaders genHeaders(HttpServletRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        Enumeration<String> it =  request.getHeaderNames();
+        while (it.hasMoreElements()) {
+            String headName = it.nextElement();
+            headers.addAll(headName, Collections.list(request.getHeaders(headName)));
+        }
+        return headers;
+    }
     private void fillResponse(HttpServletResponse response, ResponseEntity<String> returnValue) {
         response.setStatus(returnValue.getStatusCodeValue());
         response.setHeader("Content-type", "text/html;charset=UTF-8");
