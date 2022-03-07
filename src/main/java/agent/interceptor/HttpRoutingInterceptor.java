8067ef2bde28d8e2f164e98e5b4a0a2240ffa8db
Ni Rui <nirui@datagrand.com>
2022-03-07 22:44:56 +0800

    [refactory] extract class HttpServletRequestRoutingAdapter

--- src/main/java/agent/interceptor/HttpRoutingInterceptor.java
+++ src/main/java/agent/interceptor/HttpRoutingInterceptor.java
@@ -1,24 +1,15 @@
 package agent.interceptor;
 
+import agent.domain.HttpServletRequestRoutingAdapter;
 import agent.service.HttpRoutingService;
-import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.util.StreamUtils;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.io.Writer;
-import java.net.URLDecoder;
-import java.util.*;
 
 import static utils.AnsiString.green;
 import static utils.AnsiString.red;
@@ -33,79 +24,18 @@ public class HttpRoutingInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String targetUrl = routingService.routeUrl(request);
-        if (null == targetUrl) {
+        if (!routingService.needRoute(request)) {
             log(red("match routing rule failed! " + request.getRequestURI()));
             return true;
         }
-        targetUrl += genQueryString(request);
-        log(green(String.format("match routing rule success: %s ==>> %s", request.getRequestURI(), targetUrl)));
-
-        HttpMethod method = HttpMethod.valueOf(request.getMethod());
-        HttpHeaders headers = genHeaders(request);
-
-        Object body = null;
-        try {
-            body = genHttpRequestBody(request);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        HttpEntity<Object> requestEntity = new HttpEntity<Object>(body, headers);
-
-        ResponseEntity<String> returnValue = null;
-        try {
-            returnValue = new RestTemplate().exchange(targetUrl, method, requestEntity, String.class);
-        } catch (HttpStatusCodeException e) {
-            returnValue = new ResponseEntity<String>(e.getResponseBodyAsString(), e.getStatusCode());
-            e.printStackTrace();
-        } catch (ResourceAccessException e) {
-            returnValue = new ResponseEntity<String>("Connection refused! Can't find service(s)", HttpStatus.BAD_GATEWAY);
-            e.printStackTrace();
-        } catch (RestClientException e) {
-            returnValue = new ResponseEntity<String>("Unknown Error!", HttpStatus.INTERNAL_SERVER_ERROR);
-            e.printStackTrace();
-            throw e;
-        } finally {
-            fillResponse(response, returnValue);
-            log("exchanged! http code is [" + response.getStatus() + "]");
-        }
+        HttpServletRequestRoutingAdapter req = new HttpServletRequestRoutingAdapter(request, routingService.findRoutingRule(request));
+        log(green(String.format("match routing rule success: %s ==>> %s", request.getRequestURI(), req.getTotalUrl())));
+        ResponseEntity<String> returnValue = routingService.route(req);
+        fillResponse(response, returnValue);
+        log("exchanged! http code is [" + response.getStatus() + "]");
         return false;
     }
 
-    private Object genHttpRequestBody(HttpServletRequest request) throws IOException {
-        if (request instanceof StandardMultipartHttpServletRequest) {
-            MultiValueMap<String, Object> formatBody = new LinkedMultiValueMap<String, Object>();
-            for (Map.Entry<String, String[]> paramEntity : request.getParameterMap().entrySet()) {
-                formatBody.addAll(paramEntity.getKey(), Arrays.asList(paramEntity.getValue()));
-            }
-            return formatBody;
-        } else {
-            return StreamUtils.copyToByteArray(request.getInputStream());
-        }
-    }
-
-    private String genQueryString(HttpServletRequest request) {
-        String queryStr = request.getQueryString();
-        if (null == queryStr || 0 == queryStr.length()) {
-            return "";
-        }
-
-        try {
-            queryStr = URLDecoder.decode(queryStr, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return String.format("?%s", queryStr);
-    }
-    private HttpHeaders genHeaders(HttpServletRequest request) {
-        HttpHeaders headers = new HttpHeaders();
-        Enumeration<String> it =  request.getHeaderNames();
-        while (it.hasMoreElements()) {
-            String headName = it.nextElement();
-            headers.addAll(headName, Collections.list(request.getHeaders(headName)));
-        }
-        return headers;
-    }
     private void fillResponse(HttpServletResponse response, ResponseEntity<String> returnValue) {
         response.setStatus(returnValue.getStatusCodeValue());
         response.setHeader("Content-type", "text/html;charset=UTF-8");
--- src/main/java/agent/service/HttpRoutingService.java
+++ src/main/java/agent/service/HttpRoutingService.java
@@ -1,8 +1,17 @@
 package agent.service;
 
 import agent.domain.HttpRoutingRule;
+import agent.domain.HttpServletRequestRoutingAdapter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -28,6 +37,38 @@ public class HttpRoutingService {
         logSettingRoutingRules(routingRules);
     }
 
+    public boolean needRoute(HttpServletRequest request) {
+        HttpRoutingRule rule = findRoutingRule(request);
+        return null != rule;
+    }
+
+    public ResponseEntity<String> route(HttpServletRequestRoutingAdapter req) {
+        String url = req.getTotalUrl();
+        HttpMethod mtd = req.getMethod();
+        HttpEntity<Object> entity = req.getEntity();
+
+        ResponseEntity<String> returnValue;
+        try {
+            returnValue = new RestTemplate().exchange(url, mtd, entity, String.class);
+        } catch (HttpStatusCodeException e) {
+            returnValue = new ResponseEntity<String>(e.getResponseBodyAsString(), e.getStatusCode());
+            e.printStackTrace();
+        } catch (ResourceAccessException e) {
+            returnValue = new ResponseEntity<String>("Connection refused! Can't find service(s)", HttpStatus.BAD_GATEWAY);
+            e.printStackTrace();
+        } catch (RestClientException e) {
+            returnValue = new ResponseEntity<String>("Unknown Error!", HttpStatus.INTERNAL_SERVER_ERROR);
+            e.printStackTrace();
+        }
+        return returnValue;
+    }
+
+    public HttpRoutingRule findRoutingRule(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        String serviceName = uri.split("[/]")[1];
+        return routingRuleMap.get(serviceName);
+    }
+
     private void logSettingRoutingRules(List<HttpRoutingRule> routingRules) {
         // TODO try to move out as AOP
 
@@ -38,17 +79,4 @@ public class HttpRoutingService {
         System.out.println("========= init routing rules end =========");
     }
 
-    public String routeUrl(HttpServletRequest request) {
-        String uri = request.getRequestURI();
-        HttpRoutingRule rule = findRoutingRule(uri);
-        if (null == rule) {
-            return null;
-        }
-        return rule.routeUrl(uri);
-    }
-
-    private HttpRoutingRule findRoutingRule(String uri) {
-        String serviceName = uri.split("[/]")[1];
-        return routingRuleMap.get(serviceName);
-    }
 }
