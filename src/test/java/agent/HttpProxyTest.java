b33552972d18b4bb57c49235b04bc3a431cd1ae4
Ni Rui <nirui@datagrand.com>
2022-05-25 21:41:15 +0800

    [test case] add test cases for HttpProxy

---
+++ src/test/java/agent/HttpProxyTest.java
@@ -0,0 +1,52 @@
+package agent;
+
+import agent.domain.HttpDispatcher;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import samples.agent.HttpProxyApplication;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HttpProxyApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpProxyTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void test_localResponse_should_ok_without_invokeHeader() {
+        ResponseEntity<String> result = testRestTemplate.exchange("/proxy/test", HttpMethod.GET, null, String.class);
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals("Hello, I am the test! [1]", result.getBody());
+    }
+
+    @Test
+    public void test_localResponse_should_notFound_without_invokeHeader() {
+        ResponseEntity<String> result = testRestTemplate.exchange("/proxy/no_such_path", HttpMethod.GET, null, String.class);
+        assertEquals(HttpStatus.NOT_FOUND, result.getStatusCode());
+    }
+
+    @Test
+    public void test_agentResponse_should_badGateway_with_realServiceStopped() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpDispatcher.INVOKE_HEADER, "flask");
+        HttpEntity<String> requestEntity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> result = testRestTemplate.exchange("/greet", HttpMethod.GET, requestEntity, String.class);
+        assertEquals(HttpStatus.BAD_GATEWAY, result.getStatusCode());
+    }
+    @Test
+    public void test_localResponse_should_notFound_with_invalidInvokeHeader() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpDispatcher.INVOKE_HEADER, "noServer");
+        HttpEntity<String> requestEntity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> result = testRestTemplate.exchange("/greet", HttpMethod.GET, requestEntity, String.class);
+        assertEquals(HttpStatus.NOT_FOUND, result.getStatusCode());
+    }
+
+}
